1101247e8f1cf1d695bfa7d01062b5f5d92d7c6d
Oriana Acosta <orianaantonellaacosta@frlp.utn.edu.ar>
2026-05-17 16:17:37 -0300

    feat(frontend): agrega rutas y link de acceso en el Layout

--- packages/web/src/Layout.tsx
+++ packages/web/src/Layout.tsx
@@ -33,6 +33,20 @@ function Layout() {
                                 Miembros
                             </Text>
                         </RouterLink>
+
+                        {/* === LINK AGREGADO: LOCKERS === */}
+                        <RouterLink to="/lockers">
+                            <Text 
+                                fontWeight="semibold" 
+                                fontSize="sm" 
+                                textTransform="uppercase" 
+                                letterSpacing="wider"
+                                color="fg.muted"
+                                _hover={{ color: "blue.500", textDecoration: "none" }}
+                            >
+                                Lockers
+                            </Text>
+                        </RouterLink>
                     </HStack>
                 </Flex>
             </Box>
