7fc512668f5ab18869c29979a607f7d2e588b717
juan-caceres <jcruz2013c@gmail.com>
2026-05-23 20:06:47 -0300

    feat(backend): agrega test unitario creacion de deporte exitoso

---
+++ packages/api/src/application/NewSportUseCase.test.ts
@@ -0,0 +1,73 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { CreateSportUseCase } from './NewSportUseCase.js';
+import { SportRepository } from '../domain/SportRepository.js';
+import { SportValidator } from '../domain/services/SportValidator.js';
+import { CreateSportRequest } from '@alentapp/shared';
+import { mock } from 'node:test';
+
+describe('CreateSportUseCase', () => {
+    //Creacion de Moks de nuestras dependencias
+    const mockSportRepo = { //simula acceso a la base de datos (repositorio)  para crear deporte
+        create: vi.fn(),
+    } as unknown as SportRepository; //para que lo conozca como SportRepository
+
+    //simula las validaciones para el deporte
+    const mockSportValidator = {
+        validateNameIsUnique: vi.fn(),
+        validateMaxCapacity: vi.fn(),
+    } as unknown as SportValidator; //para que lo conozca como SportValidator
+
+    //Instanciamos el caso de uso inyectando los mocks
+    const useCase = new CreateSportUseCase(mockSportRepo, mockSportValidator);
+
+    beforeEach(() => {
+        vi.clearAllMocks(); //limpia los mocks antes de cada test para evitar interferencias
+    });
+
+    //Test Unitario para el caso de uso de creacion de deporte
+    it ('debe crear un deporte exitosamente si pasa validaciones', async () => {
+        const mockRequest: CreateSportRequest = { //datos de entrada para crear un deporte
+            name: 'Futbol',
+            description: 'Deporte de 11 vs 11',
+            max_capacity: 20,
+            additional_price: 500,
+            requires_medical_certificate: true
+        };
+
+        //simulamos que el nombre del deporte es unico
+        vi.mocked(mockSportValidator.validateNameIsUnique).mockResolvedValue(undefined);
+        
+        //simulamos que la capacidad es valida
+        vi.mocked(mockSportValidator.validateMaxCapacity).mockReturnValue(undefined); 
+        
+        //usamos undefinded porque la validacion no retorna nada si es exitosa
+    
+        //Simulamos respuesta de exito de la base de datos
+        vi.mocked(mockSportRepo.create).mockResolvedValueOnce({
+            id: 'uuid-sport-123',
+            ...mockRequest,
+            created_at: '2026-04-28T00:00:00.000Z'
+        });
+
+        const result = await useCase.execute(mockRequest); //ejecutamos el caso de uso con los datos de entrada
+    
+        //VERIFICACIONES
+        
+        //Comprobamos que se llamo al validador con los datos correctos
+        expect(mockSportValidator.validateNameIsUnique).toHaveBeenCalledWith(mockRequest.name);
+        expect(mockSportValidator.validateMaxCapacity).toHaveBeenCalledWith(mockRequest.max_capacity);
+
+        //Comprobamos que se intento persistir los datos con el repositorio
+        expect(mockSportRepo.create).toHaveBeenCalledWith(expect.objectContaining({
+            name: 'Futbol',
+            max_capacity: 20,
+            requires_medical_certificate: true
+        })); //no colocamos todos los campos para no duplicar codigo y nos enfocamos en los mas relevantes para la logica de negocio
+        
+
+        //Comprobamos el retorno del caso de uso
+        expect(result.id).toBe('uuid-sport-123');
+        expect(result.name).toBe('Futbol');
+    });
+
+});
